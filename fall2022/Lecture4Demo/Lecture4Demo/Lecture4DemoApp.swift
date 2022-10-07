9287974d4d93e676b162bece07d33eda31a6a244
AndyyHuang <handy1221@berkeley.edu>
2022-10-07 02:14:20 -0700

    renamed a folder

--- fall2022/Lecture4Demo/Lecture4Demo/Lecture4DemoApp.swift
+++ fall2022/Lecture4Demo/Lecture4Demo/Lecture4DemoApp.swift
@@ -1,14 +1,14 @@
 //
-//  Lecture3DemoApp.swift
-//  Lecture3Demo
+//  Lecture4DemoApp.swift
+//  Lecture4Demo
 //
-//  Created by Andy Huang on 10/6/22.
+//  Created by Andy Huang on 10/7/22.
 //
 
 import SwiftUI
 
 @main
-struct Lecture3DemoApp: App {
+struct Lecture4DemoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
