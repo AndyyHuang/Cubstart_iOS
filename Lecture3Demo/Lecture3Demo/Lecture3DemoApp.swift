0312d298796caa3b345d4b9d0b5ce5f76bc7a422
AndyyHuang <handy1221@berkeley.edu>
2022-10-07 01:59:36 -0700

    lecture 3 demo

---
+++ Lecture3Demo/Lecture3Demo/Lecture3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture3DemoApp.swift
+//  Lecture3Demo
+//
+//  Created by Andy Huang on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Lecture3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
